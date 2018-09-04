fcc47a6f2e05072dbb006555cda4b7e825b37d68
Hudson Schumaker <hudson@schumakerteam.com>
2018-09-03 23:07:09 -0300

    Herança

---
+++ src/br/com/schumaker/hudson/oo/heranca/Funcionario.java
@@ -0,0 +1,27 @@
+package br.com.schumaker.hudson.oo.heranca;
+
+/**
+ *
+ * @author hudsonschumaker
+ */
+public class Funcionario extends Pessoa {
+
+    private float salario;
+    private String matricula;
+
+    public float getSalario() {
+        return salario;
+    }
+
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+}

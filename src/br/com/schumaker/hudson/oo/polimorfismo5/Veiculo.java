dfe5e9f842a819a33c480c36b0416a276958876a
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2021-08-25 19:32:26 +0200

    Update Veiculo.java

--- src/br/com/schumaker/hudson/oo/polimorfismo5/Veiculo.java
+++ src/br/com/schumaker/hudson/oo/polimorfismo5/Veiculo.java
@@ -2,7 +2,7 @@ package br.com.schumaker.hudson.oo.polimorfismo5;
 
 /**
  *
- * @author Hudson.schumaker
+ * @author Hudson schumaker
  */
 public abstract class Veiculo {
     abstract void listarVerificacoes();
